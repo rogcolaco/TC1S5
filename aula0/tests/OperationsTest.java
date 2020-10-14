63c2cdfcfd9904367d43f44add27266cb3e7b422
Rogerio Colaço <rcs@ufscar.br>
2020-10-14 20:51:45 -0300

    - Use BeforeAll and  DisplayName

--- aula0/tests/OperationsTest.java
+++ aula0/tests/OperationsTest.java
@@ -1,26 +1,45 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperationsTest {
 
+    static Operations p;
+
+    @BeforeAll
+    public static void  createObject(){
+        p = new Operations();
+    }
+
     @Test
+    @DisplayName("Teste do método adição da classe Operations")
     void adicaoTest() {
-        Operations p = new Operations();
         assertEquals(10.0f,p.adicao(3.0f,7.0f));
     }
 
     @Test
+    @DisplayName("Teste do método subtração da classe Operations")
     void subtracaoTest() {
-        Operations p = new Operations();
         assertEquals(10.f,p.subtracao(13.0f,3.0f));
     }
 
     @Test
+    @DisplayName("Teste do método multiplicação da classe Operations")
     void multiplicacaoTest() {
+        assertEquals(6.0f,p.multiplicacao(3.0f,2.0f));
     }
 
     @Test
+    @DisplayName("Teste do método resto da classe Operations com assertEquals")
     void restoTest() {
+        assertEquals(1,p.resto(5,2));
+    }
+
+    @Test
+    @DisplayName("Teste do método resto da classe Operations com assertNotEquals")
+    void restoTest2(){
+        assertNotEquals(1,10,2);
     }
 }
