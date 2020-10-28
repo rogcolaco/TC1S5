5c5b91353e74353d5dff668c93fd2feab86de55c
Rogerio Colaço <rcs@ufscar.br>
2020-10-28 20:50:40 -0300

    - Ordenação de caso de teste;

--- aula04/Tests/MainTest.java
+++ aula04/Tests/MainTest.java
@@ -1,8 +1,8 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.Random.class)
 
 class MainTest {
     private static Main p;
