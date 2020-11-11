cbc10d411e5c03f8a181a4f5fe019e73e10dad0c
Rogerio Colaço <rcs@ufscar.br>
2020-11-11 20:03:17 -0300

    - RepeatedTest;

--- aula05_01/Test/VeiculoTest.java
+++ aula05_01/Test/VeiculoTest.java
@@ -1,14 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VeiculoTest {
 
+    public static ArrayList<Veiculo> listaVeiculos;
+
+    @BeforeAll
+    public static void criaArrayList(){
+        listaVeiculos = new ArrayList<>();
+
+    }
+
     @Test
+    @RepeatedTest(10)
     @DisplayName("Teste getnroRodas")
     void test() {
         Veiculo v = new Veiculo("aquatico","abcde", 0, 200);
+        listaVeiculos.add(v);
         assertEquals(0, v.getNroRodas());
     }
 
